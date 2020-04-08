3ae2f65df4431a7635d56aad4ee871398030cfa4
Mao Li <mal131@ucsd.edu>
2020-04-07 23:41:49 -0700

    Added: frontend part of the demo text analytics page

--- debatestar/demo.py
+++ debatestar/demo.py
@@ -6,7 +6,12 @@ from flask import (
 
 bp = Blueprint('demo', __name__)
 
-# View for the Demo: show the persuasivenss of given debate
+# View for the Demo: show the persuasivenss of given debate video
 @bp.route('/demo')
 def demo():
     return render_template('demo.html')
+
+# View for the text: show the persuasivenss rating for user input debate script
+@bp.route('/text')
+def text():
+    return render_template('text.html')
