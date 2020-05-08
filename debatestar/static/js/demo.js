ffc95401e0855bd5ca23022c0d30b9f48f13eab3
Mao Li <mal131@ucsd.edu>
2020-05-07 19:08:35 -0700

    Added: dummy hardcoded version of dynamically updated scores

--- debatestar/static/js/demo.js
+++ debatestar/static/js/demo.js
@@ -10,6 +10,9 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var videotime = 0;
 var timeupdater = null; 
 
+// Array to store the Persuasiveness scores. 
+var scoresArr = [];
+
 // Load the player when API is ready
 var player;
 function onYouTubeIframeAPIReady() {
@@ -26,9 +29,11 @@ function onYouTubeIframeAPIReady() {
 function onPlayerReady(event) {
     console.log("player is ready");
     //timeupdater = setInterval(updateTime, 1000);
+    fillScoresArr(scoresArr);
 }
 
 // On Play/Pause, udpate the time inteval
+// 1000ms = 1s
 function onPlayerStateChange(event) {
     console.log("player state changed");
 
@@ -53,7 +58,29 @@ function updateTime() {
     }
 }
 
+// TODO: this hardcoded version won't be used
+function fillScoresArr(scoresArr) {
+    const max = 0.76;
+    const min = 0.39;
+    // intro part
+    for (var i = 0; i < 240; i++)
+        scoresArr.push(0.5);
+    // begin the debate
+    for (var i = 241; i < 4472; i++)
+        scoresArr.push(Math.random() * (max - min) + min);
+    // end part
+    for (var i = 4473; i < 5000; i++)
+        scoresArr.push(scoresArr[4470]);
+    console.log("Score Array Generated");
+}
+
 // update the scores
 function updateScore(videotime) {
-    console.log("update score " + Math.round(videotime));
+    //console.log("update score " + Math.round(videotime));
+    var forrate = scoresArr[Math.round(videotime)] * 100;
+    forrate = forrate.toFixed(2);
+    var againstrate = 100 - forrate;
+    againstrate = againstrate.toFixed(2);
+    document.getElementById("forrate").textContent = forrate.toString() + "%";
+    document.getElementById("againstrate").textContent = againstrate.toString() + "%";
 }
